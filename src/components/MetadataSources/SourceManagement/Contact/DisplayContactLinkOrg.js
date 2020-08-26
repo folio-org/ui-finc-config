6e20595db74e35920d72b00fe76940edb077caf7
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2020-08-26 11:14:31 +0200

    UIFC-209-re-design-contacts get links working to users and organizations

---
+++ src/components/MetadataSources/SourceManagement/Contact/DisplayContactLinkOrg.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import { stripesConnect } from '@folio/stripes/core';
+
+import urls from '../../../DisplayUtils/urls';
+
+class DisplayContactLinkOrg extends React.Component {
+  static manifest = Object.freeze({
+    org: {
+      type: 'okapi',
+      path: 'organizations-storage/organizations/!{contactId}',
+      throwErrors: false
+    },
+    query: {},
+  });
+
+  static propTypes = {
+    resources: PropTypes.shape({
+      org: PropTypes.object,
+      failed: PropTypes.object,
+    }).isRequired,
+    stripes: PropTypes.shape({
+      okapi: PropTypes.object
+    }),
+    contact: PropTypes.object,
+    contactId: PropTypes.string,
+  };
+
+  getContactForOrganization = (contact, contactId) => {
+    if (contact.type === 'contact' && this.props.resources.org && this.props.resources.org.failed) {
+      if (contact.name) {
+        return contact.name;
+      } else {
+        return '-';
+      }
+    } else {
+      return (
+        <React.Fragment>
+          <Link to={{
+            pathname: `${urls.organizationView(contactId)}`,
+          }}
+          >
+            {contact.name}
+          </Link>
+        </React.Fragment>
+      );
+    }
+  }
+
+  render() {
+    const { contact, contactId } = this.props;
+    const contactNameWithLink = this.getContactForOrganization(contact, contactId);
+
+    return (
+      <React.Fragment>
+        {contactNameWithLink}
+      </React.Fragment>
+    );
+  }
+}
+
+export default stripesConnect(DisplayContactLinkOrg);
