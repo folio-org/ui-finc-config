eae7599ab7256674e9e0f6054a98b48a58c2bdc0
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2020-09-10 15:23:26 +0200

    remove-unused-code

--- src/components/MetadataSources/MetadataSourceForm.js
+++ src/components/MetadataSources/MetadataSourceForm.js
@@ -35,12 +35,12 @@ class MetadataSourceForm extends React.Component {
     initialValues: PropTypes.object,
     invalid: PropTypes.bool,
     isLoading: PropTypes.bool,
-    onCancel: PropTypes.func,
+    // onCancel: PropTypes.func,
     onDelete: PropTypes.func,
-    onSubmit: PropTypes.func,
+    // onSubmit: PropTypes.func,
     pristine: PropTypes.bool,
     submitting: PropTypes.bool,
-    stripes: PropTypes.object,
+    // stripes: PropTypes.object,
   };
 
   static defaultProps = {
