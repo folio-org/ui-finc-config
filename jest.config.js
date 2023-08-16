20519d1876adad5b99e4c3e9f529e1cabdfbf295
alb3rtino <alb3rtino@users.noreply.github.com>
2023-08-16 22:40:26 +0200

    Fix test environment

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
 const config = require('@folio/jest-config-stripes');
 
+const esModules = [
+  '@folio',
+  '@k-int',
+  'ky',
+  'uuid'
+].join('|');
+
 module.exports = {
   ...config,
   setupFiles: [
     ...config.setupFiles,
     path.join(__dirname, './test/jest/setupFiles.js'),
   ],
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
 };
