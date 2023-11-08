0c84fd32fe43ccb080730b3f6790ebbaad663c17
elsenhans <elsenhans@users.noreply.github.com>
2023-11-08 16:03:46 +0100

    UIFC-338-use-functional-components-instead-of-class-components Navigation

--- src/components/Navigation/FincNavigation.js
+++ src/components/Navigation/FincNavigation.js
@@ -9,35 +9,33 @@ import {
 
 import urls from '../DisplayUtils/urls';
 
-class FincNavigation extends React.Component {
-  static propTypes = {
-    id: PropTypes.string,
-  };
+const FincNavigation = ({
+  id,
+}) => {
+  return (
+    <ButtonGroup fullWidth data-test-navigation>
+      <Button
+        buttonStyle={id === 'source' ? 'primary' : 'default'}
+        data-test-navigation-source
+        id="metadata-sources"
+        to={id === 'collection' ? urls.sources() : ''}
+      >
+        <FormattedMessage id="ui-finc-config.navigation.sources" />
+      </Button>
+      <Button
+        buttonStyle={id === 'collection' ? 'primary' : 'default'}
+        data-test-navigation-collection
+        id="metadata-collections"
+        to={id === 'source' ? urls.collections() : ''}
+      >
+        <FormattedMessage id="ui-finc-config.navigation.collections" />
+      </Button>
+    </ButtonGroup>
+  );
+};
 
-  render() {
-    const { id } = this.props;
-
-    return (
-      <ButtonGroup fullWidth data-test-navigation>
-        <Button
-          buttonStyle={id === 'source' ? 'primary' : 'default'}
-          data-test-navigation-source
-          id="metadata-sources"
-          to={id === 'collection' ? urls.sources() : ''}
-        >
-          <FormattedMessage id="ui-finc-config.navigation.sources" />
-        </Button>
-        <Button
-          buttonStyle={id === 'collection' ? 'primary' : 'default'}
-          data-test-navigation-collection
-          id="metadata-collections"
-          to={id === 'source' ? urls.collections() : ''}
-        >
-          <FormattedMessage id="ui-finc-config.navigation.collections" />
-        </Button>
-      </ButtonGroup>
-    );
-  }
-}
+FincNavigation.propTypes = {
+  id: PropTypes.string,
+};
 
 export default FincNavigation;
