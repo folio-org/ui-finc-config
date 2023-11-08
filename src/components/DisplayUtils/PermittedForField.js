3bde3b5596ed0f24ca3f0cdf7287bff84d9a651e
elsenhans <elsenhans@users.noreply.github.com>
2023-11-08 15:39:57 +0100

    UIFC-338-use-functional-components-instead-of-class-components add hook usePrevious

--- src/components/DisplayUtils/PermittedForField.js
+++ src/components/DisplayUtils/PermittedForField.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Field } from 'react-final-form';
 import {
@@ -15,64 +15,69 @@ import {
 } from '@folio/stripes/components';
 
 import { Required } from './Validate';
+import usePrevious from '../hooks/usePrevious';
 
-class PermittedForField extends React.Component {
-  static propTypes = {
-    addPermittedForField: PropTypes.bool,
-    ariaLabel: PropTypes.string,
-    disable: PropTypes.bool,
-    fields: PropTypes.object,
-    intl: PropTypes.shape({
-      formatMessage: PropTypes.func.isRequired,
-    }),
-  };
+const PermittedForField = ({
+  addPermittedForField,
+  ariaLabel,
+  disable,
+  fields,
+  intl,
+}) => {
+  const prevAddPermittedForField = usePrevious(addPermittedForField) || [];
 
-  componentDidUpdate(prevProps) {
+  useEffect(() => {
     // add first required permitted-for-field, if usageRestricted changed to 'yes'
-    if (this.props.addPermittedForField !== prevProps.addPermittedForField) {
-      if (this.props.addPermittedForField) {
-        this.props.fields.push('');
+    if (prevAddPermittedForField !== addPermittedForField) {
+      if (addPermittedForField) {
+        fields.push('');
       }
     }
-  }
+  });
 
-  render() {
-    const { fields, ariaLabel, disable, intl } = this.props;
+  return (
+    <Row>
+      <Col xs={12}>
+        {fields.map((elem, index) => (
+          <Row key={index}>
+            <Col xs={8}>
+              <Field
+                ariaLabel={`${ariaLabel} #${parseInt(index + 1, 10)}`}
+                component={TextField}
+                fullWidth
+                id={elem}
+                name={elem}
+                placeholder={intl.formatMessage({ id: 'ui-finc-config.collection.placeholder.permittedFor' })}
+                // first field is required
+                validate={index === 0 ? Required : undefined}
+              />
+            </Col>
+            <Col xs={1}>
+              {/* no trash icon for first required field */}
+              {index !== 0 ?
+                <IconButton
+                  icon="trash"
+                  onClick={index !== 0 ? () => fields.remove(index) : undefined}
+                /> : ''}
+            </Col>
+          </Row>
+        ))}
+      </Col>
+      <Col xs={4}>
+        <Button onClick={() => fields.push('')} disabled={disable}><FormattedMessage id="ui-finc-config.form.button.add" /></Button>
+      </Col>
+    </Row>
+  );
+};
 
-    return (
-      <Row>
-        <Col xs={12}>
-          {fields.map((elem, index) => (
-            <Row key={index}>
-              <Col xs={8}>
-                <Field
-                  ariaLabel={`${ariaLabel} #${parseInt(index + 1, 10)}`}
-                  component={TextField}
-                  fullWidth
-                  id={elem}
-                  name={elem}
-                  placeholder={intl.formatMessage({ id: 'ui-finc-config.collection.placeholder.permittedFor' })}
-                  // first field is required
-                  validate={index === 0 ? Required : undefined}
-                />
-              </Col>
-              <Col xs={1}>
-                {/* no trash icon for first required field */}
-                {index !== 0 ?
-                  <IconButton
-                    icon="trash"
-                    onClick={index !== 0 ? () => fields.remove(index) : undefined}
-                  /> : ''}
-              </Col>
-            </Row>
-          ))}
-        </Col>
-        <Col xs={4}>
-          <Button onClick={() => fields.push('')} disabled={disable}><FormattedMessage id="ui-finc-config.form.button.add" /></Button>
-        </Col>
-      </Row>
-    );
-  }
-}
+PermittedForField.propTypes = {
+  addPermittedForField: PropTypes.bool,
+  ariaLabel: PropTypes.string,
+  disable: PropTypes.bool,
+  fields: PropTypes.object,
+  intl: PropTypes.shape({
+    formatMessage: PropTypes.func.isRequired,
+  }),
+};
 
 export default injectIntl(PermittedForField);
--- src/components/DisplayUtils/RepeatableFieldValidUrl.js
+++ src/components/DisplayUtils/RepeatableFieldValidUrl.js
@@ -13,47 +13,47 @@ import {
 
 import { ValidateUrl } from './Validate';
 
-class RepeatableFieldValidUrl extends React.Component {
-  static propTypes = {
-    ariaLabel: PropTypes.string,
-    fields: PropTypes.object,
-    placeholder: PropTypes.string,
-  };
+const RepeatableFieldValidUrl = ({
+  ariaLabel,
+  fields,
+  placeholder
+}) => {
+  return (
+    <Row>
+      <Col xs={12}>
+        {fields.map((elem, index) => (
+          <Row key={index}>
+            <Col xs={8}>
+              <Field
+                ariaLabel={`${ariaLabel} #${parseInt(index + 1, 10)}`}
+                component={TextField}
+                fullWidth
+                id={elem}
+                name={elem}
+                placeholder={placeholder}
+                validate={ValidateUrl}
+              />
+            </Col>
+            <Col xs={1}>
+              <IconButton
+                icon="trash"
+                onClick={() => fields.remove(index)}
+              />
+            </Col>
+          </Row>
+        ))}
+      </Col>
+      <Col xs={4}>
+        <Button onClick={() => fields.push('')}><FormattedMessage id="ui-finc-config.form.button.add" /></Button>
+      </Col>
+    </Row>
+  );
+};
 
-  render() {
-    const { fields, ariaLabel, placeholder } = this.props;
-
-    return (
-      <Row>
-        <Col xs={12}>
-          {fields.map((elem, index) => (
-            <Row key={index}>
-              <Col xs={8}>
-                <Field
-                  ariaLabel={`${ariaLabel} #${parseInt(index + 1, 10)}`}
-                  component={TextField}
-                  fullWidth
-                  id={elem}
-                  name={elem}
-                  placeholder={placeholder}
-                  validate={ValidateUrl}
-                />
-              </Col>
-              <Col xs={1}>
-                <IconButton
-                  icon="trash"
-                  onClick={() => fields.remove(index)}
-                />
-              </Col>
-            </Row>
-          ))}
-        </Col>
-        <Col xs={4}>
-          <Button onClick={() => fields.push('')}><FormattedMessage id="ui-finc-config.form.button.add" /></Button>
-        </Col>
-      </Row>
-    );
-  }
-}
+RepeatableFieldValidUrl.propTypes = {
+  ariaLabel: PropTypes.string,
+  fields: PropTypes.object,
+  placeholder: PropTypes.string,
+};
 
 export default RepeatableFieldValidUrl;
