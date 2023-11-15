91f7ba7dba55d762aeac65456a8421d5d2328a53
elsenhans <elsenhans@users.noreply.github.com>
2023-11-15 14:28:06 +0100

    UIFC-338-use-functional-components-instead-of-class-components FindContact, FindUser

--- src/components/MetadataSources/SourceManagement/FindContact/FindContact.js
+++ src/components/MetadataSources/SourceManagement/FindContact/FindContact.js
@@ -10,55 +10,53 @@ import { Pluggable } from '@folio/stripes/core';
 
 import BasicCss from '../../../BasicStyle.css';
 
-class FindContact extends React.Component {
-  render() {
-    const disableRecordCreation = true;
-    const buttonProps = { 'marginBottom0': true };
-    const pluggable =
-      <Pluggable
-        addContacts={this.props.selectContact}
-        aria-haspopup="true"
-        buttonProps={buttonProps}
-        columnMapping={this.columnMapping}
-        dataKey="contact"
-        disableRecordCreation={disableRecordCreation}
-        id={`clickable-find-contact ${this.props.index}`}
-        marginTop0
-        onCloseModal={(modalProps) => {
-          modalProps.parentMutator.query.update({
-            query: '',
-            filters: '',
-            sort: 'Name',
-          });
-        }}
-        searchButtonStyle="default"
-        searchLabel={<FormattedMessage id="ui-finc-config.plugin.buttonLabel.contact" />}
-        type="find-contact"
-        visibleColumns={['name', 'code', 'description']}
-        {...this.props}
-      >
-        <div style={{ background: 'red' }}><FormattedMessage id="ui-finc-config.plugin.notFound" /></div>
-      </Pluggable>;
+const FindContact = ({
+  index,
+  selectContact,
+}) => {
+  const disableRecordCreation = true;
+  const buttonProps = { 'marginBottom0': true };
+  const pluggable =
+    <Pluggable
+      addContacts={selectContact}
+      aria-haspopup="true"
+      buttonProps={buttonProps}
+      dataKey="contact"
+      disableRecordCreation={disableRecordCreation}
+      id={`clickable-find-contact ${index}`}
+      marginTop0
+      onCloseModal={(modalProps) => {
+        modalProps.parentMutator.query.update({
+          query: '',
+          filters: '',
+          sort: 'Name',
+        });
+      }}
+      searchButtonStyle="default"
+      searchLabel={<FormattedMessage id="ui-finc-config.plugin.buttonLabel.contact" />}
+      type="find-contact"
+      visibleColumns={['name', 'code', 'description']}
+    >
+      <div style={{ background: 'red' }}><FormattedMessage id="ui-finc-config.plugin.notFound" /></div>
+    </Pluggable>;
 
-    return (
-      <>
-        <Row>
-          <Label className={BasicCss.styleForFormLabel}>
-            <FormattedMessage id="ui-finc-config.source.contact" />
-          </Label>
-        </Row>
-        <Row>
-          { pluggable }
-        </Row>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Row>
+        <Label className={BasicCss.styleForFormLabel}>
+          <FormattedMessage id="ui-finc-config.source.contact" />
+        </Label>
+      </Row>
+      <Row>
+        { pluggable }
+      </Row>
+    </>
+  );
+};
 
 FindContact.propTypes = {
   index: PropTypes.number,
   selectContact: PropTypes.func,
-  stripes: PropTypes.object,
 };
 
 export default FindContact;
