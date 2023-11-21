fb87a521067bd0de8c6e2b64b8741bc05630640d
elsenhans <elsenhans@users.noreply.github.com>
2023-11-21 10:49:30 +0100

    UIFC-338-use-functional-components-instead-of-class-components index.js

--- src/index.js
+++ src/index.js
@@ -15,34 +15,43 @@ import CollectionCreateRoute from './routes/CollectionCreateRoute';
 import CollectionEditRoute from './routes/CollectionEditRoute';
 import Settings from './settings';
 
-class FincConfig extends React.Component {
-  static propTypes = {
-    match: ReactRouterPropTypes.match.isRequired,
-    showSettings: PropTypes.bool,
-  }
-
-  render() {
-    const { showSettings, match: { path } } = this.props;
-
-    if (showSettings) {
-      return <Settings {...this.props} />;
-    }
-
+const FincConfig = ({
+  match,
+  showSettings,
+  location,
+  stripes,
+}) => {
+  if (showSettings) {
     return (
-      <Switch>
-        <Route path={`${path}/metadata-sources/create`} component={SourceCreateRoute} />
-        <Route path={`${path}/metadata-sources/:id/edit`} component={SourceEditRoute} />
-        <Route path={`${path}/metadata-sources/:id?`} component={SourcesRoute}>
-          <Route path={`${path}/metadata-sources/:id`} component={SourceViewRoute} />
-        </Route>
-        <Route path={`${path}/metadata-collections/create`} component={CollectionCreateRoute} />
-        <Route path={`${path}/metadata-collections/:id/edit`} component={CollectionEditRoute} />
-        <Route path={`${path}/metadata-collections/:id?`} component={CollectionsRoute}>
-          <Route path={`${path}/metadata-collections/:id`} component={CollectionViewRoute} />
-        </Route>
-      </Switch>
+      <Settings
+        location={location}
+        match={match}
+        stripes={stripes}
+      />
     );
   }
-}
+
+  return (
+    <Switch>
+      <Route path={`${match.path}/metadata-sources/create`} component={SourceCreateRoute} />
+      <Route path={`${match.path}/metadata-sources/:id/edit`} component={SourceEditRoute} />
+      <Route path={`${match.path}/metadata-sources/:id?`} component={SourcesRoute}>
+        <Route path={`${match.path}/metadata-sources/:id`} component={SourceViewRoute} />
+      </Route>
+      <Route path={`${match.path}/metadata-collections/create`} component={CollectionCreateRoute} />
+      <Route path={`${match.path}/metadata-collections/:id/edit`} component={CollectionEditRoute} />
+      <Route path={`${match.path}/metadata-collections/:id?`} component={CollectionsRoute}>
+        <Route path={`${match.path}/metadata-collections/:id`} component={CollectionViewRoute} />
+      </Route>
+    </Switch>
+  );
+};
+
+FincConfig.propTypes = {
+  match: ReactRouterPropTypes.match.isRequired,
+  showSettings: PropTypes.bool,
+  location: PropTypes.object.isRequired,
+  stripes: PropTypes.object.isRequired,
+};
 
 export default FincConfig;
