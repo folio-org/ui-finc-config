8902f4b317ac93e2ec7ad7448cc4839bfaeda182
elsenhans <elsenhans@users.noreply.github.com>
2023-11-28 10:18:14 +0100

    UIFC-338-use-functional-components-instead-of-class-components CreateRoute

--- src/routes/CollectionCreateRoute.js
+++ src/routes/CollectionCreateRoute.js
@@ -7,78 +7,72 @@ import { stripesConnect } from '@folio/stripes/core';
 import MetadataCollectionForm from '../components/MetadataCollections/MetadataCollectionForm';
 import urls from '../components/DisplayUtils/urls';
 
-class CollectionCreateRoute extends React.Component {
-  static manifest = Object.freeze({
-    collections: {
-      type: 'okapi',
-      path: 'finc-config/metadata-collections',
-      fetch: false,
-      shouldRefresh: () => false,
-    },
-  });
+const CollectionCreateRoute = ({
+  history,
+  location,
+  mutator,
+  stripes,
+}) => {
+  const hasPerms = stripes.hasPerm('finc-config.metadata-collections.item.post');
 
-  static propTypes = {
-    history: PropTypes.shape({
-      push: PropTypes.func.isRequired,
-    }).isRequired,
-    location: PropTypes.shape({
-      search: PropTypes.string.isRequired,
-    }).isRequired,
-    mutator: PropTypes.shape({
-      collections: PropTypes.shape({
-        POST: PropTypes.func.isRequired,
-      }).isRequired,
-    }).isRequired,
-    resources: PropTypes.shape({
-      collection: PropTypes.object,
-    }).isRequired,
-    stripes: PropTypes.shape({
-      hasPerm: PropTypes.func.isRequired,
-      okapi: PropTypes.object.isRequired,
-    }).isRequired,
-  }
+  const handleClose = () => {
+    history.push(`${urls.collections()}${location.search}`);
+  };
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      hasPerms: props.stripes.hasPerm('finc-config.metadata-collections.item.post'),
-    };
-  }
-
-  handleClose = () => {
-    const { location } = this.props;
-    this.props.history.push(`${urls.collections()}${location.search}`);
-  }
-
-  handleSubmit = (collection) => {
-    const { history, location, mutator } = this.props;
-
+  const handleSubmit = (collection) => {
     mutator.collections
       .POST(collection)
       .then(({ id }) => {
         history.push(`${urls.collectionView(id)}${location.search}`);
       });
-  }
+  };
 
-  getInitialSolrMegaCollection = () => {
+  const getInitialSolrMegaCollection = () => {
     // add first field for required repeatable field
     const solrMegaCollections = [''];
 
     return { solrMegaCollections };
-  }
+  };
 
-  render() {
-    if (!this.state.hasPerms) return <div><FormattedMessage id="ui-finc-config.noPermission" /></div>;
+  if (!hasPerms) return <div><FormattedMessage id="ui-finc-config.noPermission" /></div>;
 
-    return (
-      <MetadataCollectionForm
-        handlers={{ onClose: this.handleClose }}
-        initialValues={this.getInitialSolrMegaCollection()}
-        onSubmit={this.handleSubmit}
-      />
-    );
-  }
-}
+  return (
+    <MetadataCollectionForm
+      handlers={{ onClose: handleClose }}
+      initialValues={getInitialSolrMegaCollection()}
+      onSubmit={handleSubmit}
+    />
+  );
+};
+
+CollectionCreateRoute.manifest = Object.freeze({
+  collections: {
+    type: 'okapi',
+    path: 'finc-config/metadata-collections',
+    fetch: false,
+    shouldRefresh: () => false,
+  },
+});
+
+CollectionCreateRoute.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  location: PropTypes.shape({
+    search: PropTypes.string.isRequired,
+  }).isRequired,
+  mutator: PropTypes.shape({
+    collections: PropTypes.shape({
+      POST: PropTypes.func.isRequired,
+    }).isRequired,
+  }).isRequired,
+  resources: PropTypes.shape({
+    collections: PropTypes.object,
+  }).isRequired,
+  stripes: PropTypes.shape({
+    hasPerm: PropTypes.func.isRequired,
+    okapi: PropTypes.object.isRequired,
+  }).isRequired,
+};
 
 export default stripesConnect(CollectionCreateRoute);
--- src/routes/SourceCreateRoute.js
+++ src/routes/SourceCreateRoute.js
@@ -7,67 +7,69 @@ import { stripesConnect } from '@folio/stripes/core';
 import MetadataSourceForm from '../components/MetadataSources/MetadataSourceForm';
 import urls from '../components/DisplayUtils/urls';
 
-class SourceCreateRoute extends React.Component {
-  static manifest = Object.freeze({
-    sources: {
-      type: 'okapi',
-      path: 'finc-config/metadata-sources',
-      fetch: false,
-      shouldRefresh: () => false,
-    },
-  });
+const SourceCreateRoute = ({
+  history,
+  location,
+  mutator,
+  stripes,
+}) => {
+  const hasPerms = stripes.hasPerm('finc-config.metadata-sources.item.post');
 
-  static propTypes = {
-    history: PropTypes.shape({
-      push: PropTypes.func.isRequired,
-    }).isRequired,
-    location: PropTypes.shape({
-      search: PropTypes.string.isRequired,
-    }).isRequired,
-    mutator: PropTypes.shape({
-      sources: PropTypes.shape({
-        POST: PropTypes.func.isRequired,
-      }).isRequired,
-    }).isRequired,
-    stripes: PropTypes.shape({
-      hasPerm: PropTypes.func.isRequired,
-      okapi: PropTypes.object.isRequired,
-    }).isRequired,
-  }
+  // constructor(props) {
+  //   super(props);
 
-  constructor(props) {
-    super(props);
+  //   this.state = {
+  //     hasPerms: props.stripes.hasPerm('finc-config.metadata-sources.item.post'),
+  //   };
+  // }
 
-    this.state = {
-      hasPerms: props.stripes.hasPerm('finc-config.metadata-sources.item.post'),
-    };
-  }
+  const handleClose = () => {
+    history.push(`${urls.sources()}${location.search}`);
+  };
 
-  handleClose = () => {
-    const { location } = this.props;
-    this.props.history.push(`${urls.sources()}${location.search}`);
-  }
-
-  handleSubmit = (source) => {
-    const { history, location, mutator } = this.props;
-
+  const handleSubmit = (source) => {
     mutator.sources
       .POST(source)
       .then(({ id }) => {
         history.push(`${urls.sourceView(id)}${location.search}`);
       });
-  }
+  };
 
-  render() {
-    if (!this.state.hasPerms) return <div><FormattedMessage id="ui-finc-config.noPermission" /></div>;
+  if (!hasPerms) return <div><FormattedMessage id="ui-finc-config.noPermission" /></div>;
 
-    return (
-      <MetadataSourceForm
-        handlers={{ onClose: this.handleClose }}
-        onSubmit={this.handleSubmit}
-      />
-    );
-  }
-}
+  return (
+    <MetadataSourceForm
+      handlers={{ onClose: handleClose }}
+      onSubmit={handleSubmit}
+    />
+  );
+};
+
+SourceCreateRoute.manifest = Object.freeze({
+  sources: {
+    type: 'okapi',
+    path: 'finc-config/metadata-sources',
+    fetch: false,
+    shouldRefresh: () => false,
+  },
+});
+
+SourceCreateRoute.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  location: PropTypes.shape({
+    search: PropTypes.string.isRequired,
+  }).isRequired,
+  mutator: PropTypes.shape({
+    sources: PropTypes.shape({
+      POST: PropTypes.func.isRequired,
+    }).isRequired,
+  }).isRequired,
+  stripes: PropTypes.shape({
+    hasPerm: PropTypes.func.isRequired,
+    okapi: PropTypes.object.isRequired,
+  }).isRequired,
+};
 
 export default stripesConnect(SourceCreateRoute);
