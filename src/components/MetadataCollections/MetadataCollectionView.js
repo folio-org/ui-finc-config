787f1eb8c1b2f184b94719d5a1424942fd5bf763
elsenhans <elsenhans@users.noreply.github.com>
2025-04-09 12:14:31 +0200

    UIFC-344-address-code-duplications DetailView

--- src/components/MetadataCollections/MetadataCollectionView.js
+++ src/components/MetadataCollections/MetadataCollectionView.js
@@ -1,167 +1,36 @@
-import _ from 'lodash';
 import PropTypes from 'prop-types';
-import {
-  useRef,
-  useState,
-} from 'react';
-import { FormattedMessage } from 'react-intl';
 
-import {
-  Accordion,
-  AccordionSet,
-  Button,
-  Col,
-  ExpandAllButton,
-  Icon,
-  Layout,
-  Pane,
-  PaneHeader,
-  PaneMenu,
-  Row,
-} from '@folio/stripes/components';
-import { useStripes } from '@folio/stripes/core';
-import { ViewMetaData } from '@folio/stripes/smart-components';
-
+import DetailView from '../DisplayUtils/DetailView';
 import CollectionInfoView from './CollectionInfo/CollectionInfoView';
 import CollectionManagementView from './CollectionManagement/CollectionManagementView';
 import CollectionTechnicalView from './CollectionTechnical/CollectionTechnicalView';
 
-const MetadataCollectionView = ({
-  canEdit,
-  handlers,
-  isLoading,
-  record,
-}) => {
-  const editButton = useRef();
-
-  const stripes = useStripes();
-
-  const [accordionsState, setAccordionsState] = useState({
-    managementAccordion: false,
-    technicalAccordion: false,
-  });
-
-  const handleExpandAll = (obj) => {
-    setAccordionsState(obj);
-  };
-
-  const handleAccordionToggle = ({ id }) => {
-    setAccordionsState({ ...accordionsState, [id]: !accordionsState[id] });
-  };
-
-  const renderEditPaneMenu = () => {
-    return (
-      <PaneMenu>
-        {canEdit && (
-          <Button
-            aria-label={<FormattedMessage id="ui-finc-config.edit" />}
-            buttonRef={editButton}
-            buttonStyle="primary"
-            id="clickable-edit-collection"
-            marginBottom0
-            onClick={handlers.onEdit}
-          >
-            <FormattedMessage id="ui-finc-config.edit" />
-          </Button>
-        )}
-      </PaneMenu>
-    );
-  };
-
-  const renderLoadingPanePaneHeader = () => (
-    <PaneHeader
-      dismissible
-      onClose={handlers.onClose}
-      paneTitle={<span data-test-collection-header-title>loading</span>}
-    />
-  );
-
-  const renderDetailsPanePaneHeader = (label) => (
-    <PaneHeader
-      dismissible
-      lastMenu={renderEditPaneMenu()}
-      onClose={handlers.onClose}
-      paneTitle={<span data-test-collection-header-title>{label}</span>}
-    />
-  );
-
-  const renderLoadingPane = () => {
-    return (
-      <Pane
-        defaultWidth="40%"
-        id="pane-collectiondetails"
-        renderHeader={renderLoadingPanePaneHeader}
-      >
-        <Layout className="marginTop1">
-          <Icon icon="spinner-ellipsis" width="10px" />
-        </Layout>
-      </Pane>
-    );
-  };
-
-  const label = _.get(record, 'label', 'No LABEL');
-
-  if (isLoading) return renderLoadingPane();
+const MetadataCollectionView = (props) => {
+  const accordionConfig = [
+    {
+      id: 'managementAccordion',
+      labelId: 'ui-finc-config.collection.managementAccordion',
+      Component: CollectionManagementView,
+    },
+    {
+      id: 'technicalAccordion',
+      labelId: 'ui-finc-config.collection.technicalAccordion',
+      Component: CollectionTechnicalView,
+    },
+  ];
 
   return (
-    <Pane
-      data-test-collection-pane-details
-      defaultWidth="40%"
-      id="pane-collectiondetails"
-      renderHeader={() => renderDetailsPanePaneHeader(label)}
-    >
-      <AccordionSet>
-        <ViewMetaData
-          metadata={_.get(record, 'metadata', {})}
-          stripes={stripes}
-        />
-        <CollectionInfoView
-          id="collectionInfo"
-          metadataCollection={record}
-        />
-        <Row end="xs">
-          <Col xs>
-            <ExpandAllButton
-              accordionStatus={accordionsState}
-              onToggle={handleExpandAll}
-              setStatus={null}
-            />
-          </Col>
-        </Row>
-        <Accordion
-          id="managementAccordion"
-          label={<FormattedMessage id="ui-finc-config.collection.managementAccordion" />}
-          onToggle={handleAccordionToggle}
-          open={accordionsState.managementAccordion}
-        >
-          <CollectionManagementView
-            id="collectionManagement"
-            metadataCollection={record}
-          />
-        </Accordion>
-        <Accordion
-          id="technicalAccordion"
-          label={<FormattedMessage id="ui-finc-config.collection.technicalAccordion" />}
-          onToggle={handleAccordionToggle}
-          open={accordionsState.technicalAccordion}
-        >
-          <CollectionTechnicalView
-            id="collectionTechnical"
-            metadataCollection={record}
-          />
-        </Accordion>
-      </AccordionSet>
-    </Pane>
+    <DetailView
+      accordionConfig={accordionConfig}
+      idPrefix="collection"
+      infoComponent={CollectionInfoView}
+      recordPropKey="metadataCollection"
+      {...props}
+    />
   );
 };
 
 MetadataCollectionView.propTypes = {
-  canEdit: PropTypes.bool,
-  handlers: PropTypes.shape({
-    onClose: PropTypes.func.isRequired,
-    onEdit: PropTypes.func,
-  }).isRequired,
-  isLoading: PropTypes.bool,
   record: PropTypes.object,
 };
 
